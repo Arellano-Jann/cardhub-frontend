14566cec56852975e5755553aab4f61a26e9b857
Your Name <you@example.com>
2023-04-15 06:41:52 -0700

    added some

--- src/routes/han-test/+page.svelte
+++ src/routes/han-test/+page.svelte
@@ -1,7 +1,8 @@
 <script>
+    // adds file to storage bucket
     import { initializeApp } from "firebase/app";
-    import { getFirestore } from "firebase/firestore";
-    import { getStorage, ref, uploadBytes } from "firebase/storage";
+    import { getFirestore, collection, addDoc } from "firebase/firestore";
+    import { getStorage, ref, uploadBytes, onSnapshot } from "firebase/storage";
 
     const firebaseConfig = {
         apiKey: "AIzaSyDmxG4o79Bvz8IfVJo2rKXIxMcV-08KCAQ",
@@ -18,14 +19,53 @@
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     const storage = getStorage(app);
-    const storageRef = ref(storage, 'test');
+    const storageRef = ref(storage);
+    const collectionRef = collection(db, 'cardhub-backend');
 
+    // let photo;
 
-    file = new File(["Hello, world!"], "hello.txt", {
-        type: "text/plain",
+    // function handleFileUpload(event) {
+    //     const file = event.target.files[0];
+    //     console.log(file);
+    //     photo = file;
+    //     uploadBytes(storageRef, file).then((snapshot) => {
+    //         console.log('Uploaded a file');
+    //     });
+    // }
+
+    let emptyCard = { name: "", description: "", photo: "" }; // json format
+    let card = { ...emptyCard };
+    let cards = [];
+
+    const unsubscribe = onSnapshot(doc(db, "cardhub-backend"), (card) => {
+        cards = card.docs.map((doc) => ({ id: doc.id, ...doc.data() })); // maps each card in firestore to cards variable
     });
-    uploadBytes(storageRef, file).then((snapshot) => {
-        console.log('Uploaded a file');
-    });
+
+    async function onSubmit() {
+        await addDoc(collection(db, cardhub))
+        item = { ...emptyCard }; // reset the form
+    }
+
+    async function onDelete(id) {
+        await db
+    }
 
 </script>
+
+<form on:submit|preventDefault={onSubmit}>
+    <!-- <input type="file" id="upload" on:change={handleFileUpload}>
+    <img src="photo" alt="photo"> -->
+    <input type="text" bind:value={card.name}>
+    <input type="text" bind:value={card.description}>
+    <!-- <input type="text" bind:value={card.name}> -->
+</form>
+
+<ul>
+    {#each cards as card}
+        <li>
+            <h3>{card.name}</h3>
+            <p>{card.description}</p>
+            <button on:click={() => onDelete(card.id)}>Delete</button>
+        </li>
+    {/each}
+</ul>
