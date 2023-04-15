a4098532beeafb06c5d0429ef085c3ad12754880
Mamaril-Jeremy <jmamaril@nevada.unr.edu>
2023-04-15 16:36:50 -0700

    navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,4 @@
 <script>
-    //import Home from "../../routes/home/+page.svelte";   //Idk if these are right
-    //import HostJoin from "../../routes/hostjoin/+page.svelte";
-    //import Search from "../../routes/search/+page.svelte";
-    //import About from "../../routes/about/+page.svelte";
     let Home = "https://www.google.com/";
     let HostJoin = "https://www.amazon.com/";
     let Search = "https://www.yahoo.com/";
@@ -16,45 +12,25 @@
 <nav>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <ul class="section-links">
-        <li
-            class="section-link"
-            on:click={() => setCurrentPage(Home)}
-            on:keypress={() => setCurrentPage(Home)}
-            class:selected={currentPage == Home}
-        >
+        <li class="section-link" on:click={() => setCurrentPage(Home)} on:keypress={() => setCurrentPage(Home)} class:selected={currentPage == Home}>
             <a href={Home}>
                 <span class="material-icons">home</span>
                 Home
             </a>
         </li>
-        <li
-            class="section-link"
-            on:click={() => setCurrentPage(HostJoin)}
-            on:keypress={() => setCurrentPage(HostJoin)}
-            class:selected={currentPage == HostJoin}
-        >
+        <li class="section-link" on:click={() => setCurrentPage(HostJoin)} on:keypress={() => setCurrentPage(HostJoin)} class:selected={currentPage == HostJoin}>
             <a href={HostJoin}>
                 <span class="material-icons">person_add</span>
                 Host/Join
             </a>
         </li>
-        <li
-            class="section-link"
-            on:click={() => setCurrentPage(Search)}
-            on:keypress={() => setCurrentPage(Search)}
-            class:selected={currentPage == Search}
-        >
+        <li class="section-link" on:click={() => setCurrentPage(Search)} on:keypress={() => setCurrentPage(Search)} class:selected={currentPage == Search}>
             <a href={Search}>
                 <span class="material-icons">search</span>
                 Search
             </a>
         </li>
-        <li
-            class="section-link"
-            on:click={() => setCurrentPage(About)}
-            on:keypress={() => setCurrentPage(About)}
-            class:selected={currentPage == About}
-        >
+        <li class="section-link" on:click={() => setCurrentPage(About)} on:keypress={() => setCurrentPage(About)} class:selected={currentPage == About}>
             <a href={About}>
                 <span class="material-icons">info</span>
                 About
@@ -65,55 +41,76 @@
 
 <style lang="css">
    nav {
-    position: fixed;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #1f1f1f;
-    color: #ffffff;
-    font-family: Arial, sans-serif;
-    bottom: 0;
-    width: 100%;
-    height: 50px;
+        position: fixed;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #1f1f1f;
+        color: #ffffff;
+        font-family: Arial, sans-serif;
+        bottom: 0;
+        width: 100%;
+        height: 50px;
     }
 
     .section-links {
-    display: flex;
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    justify-content: space-between;
-    width: 100%;
+        display: flex;
+        margin: 0;
+        padding: 0;
+        list-style: none;
+        justify-content: space-between;
+        width: 100%;
+        height: 100%;
     }
 
     .section-link {
-    display: flex;
-    flex-grow: 1;
-    color: white;
-    text-decoration: none;
-    -webkit-tap-highlight-color: transparent;
-    min-width: 50px;
-    overflow: hidden;
-    white-space: nowrap;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-family: sans-serif;
-    font-size: 12px;
-    transition: background-color 0.1s ease-in-out;
-    height: 100%;
+        display: flex;
+        flex-grow: 1;
+        color: white;
+        text-decoration: none;
+        -webkit-tap-highlight-color: transparent;
+        min-width: 50px;
+        overflow: hidden;
+        white-space: nowrap;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-family: sans-serif;
+        font-size: 12px;
+        transition: background-color 0.1s ease-in-out;
+        height: 100%;
     }
 
     .section-link:hover {
-    background-color: #ffffff80;
+        background-color: #ffffff80;
     }
 
     .section-link:active {
-    color: red;
+        color: red;
     }
 
     .section-link a .material-icons {
-    font-size: 25px;
+        font-size: 25px;
+    }
+    
+    @media screen and (min-width: 768px) {
+      nav {
+        top: 0;
+        bottom: auto;
+      }
+      .section-links {
+        height: 50px;
+      }
     }
 
+    @media screen and (max-width: 767px) {
+      nav {
+        top: auto;
+        bottom: 0;
+      }
+      .section-links {
+        flex-direction: column;
+        height: auto;
+      }
+    }
 </style>
