de0c742173befc17d5bb36e1b1420ed84dd327a7
brown-lyssie <abrown@nevada.unr.edu>
2023-04-15 12:07:38 -0700

    minor changes 2

--- src/routes/home/+page.server.js
+++ src/routes/home/+page.server.js
@@ -1,6 +1,7 @@
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async (event) => {
-	  // TODO log the user in
+	  // TODO join room and set name of user
+	  // from the input
 	}
-  };s
+  };
