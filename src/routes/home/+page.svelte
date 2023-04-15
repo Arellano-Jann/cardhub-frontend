601c99641e58caaf64c263d481c69e03f17791b4
brown-lyssie <abrown@nevada.unr.edu>
2023-04-15 12:07:26 -0700

    small changes to home

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Navbar from "../../lib/components/Navbar.svelte";
+</script>
+
+<!--<Navbar />-->
+<div class="home-content">
+    <div class="centered-or-smth">
+        <form method="POST">
+            <label>
+              Join Room
+              <input name="room-num" type="text">
+            </label>
+            <label>
+              Display Name
+              <input name="display-name" type="text">
+            </label>
+            <button>Join</button>
+          </form>
+    </div>
+</div>
+
+<style>
+	
+</style>
