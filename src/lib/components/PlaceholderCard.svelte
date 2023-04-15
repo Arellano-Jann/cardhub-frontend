68e01c4c86d92aa2aaf04e21fc556c992409db47
brown-lyssie <abrown@nevada.unr.edu>
2023-04-15 01:42:42 -0700

    card is lookin better

--- src/lib/components/PlaceholderCard.svelte
+++ src/lib/components/PlaceholderCard.svelte
@@ -12,13 +12,18 @@
     
 
 	import generic_card_obj from "$lib/assets/generic_card.json"
+    import { add_styles } from "svelte/internal";
 	// default card
 	export let card_obj = generic_card_obj;
 
 	//import data from "$lib/example.json.js"
 
+	
+
 </script>
 
+<!--
+	
 <div class="bg-blue-100">
 	<p>Placeholder text</p>
 	<p> {data["template_name"]}</p>
@@ -26,12 +31,32 @@
 {data["dimensions"][0]}
 <img src={img} height="{data["dimensions"][0]}px" width="{data["dimensions"][1]}px" alt="alt1">
 
-<p>separation</p>
-
 <div class="bg-red-50 test">g</div>
 
-<div class="PlaceholderCard border-0 bg-blue-50" >
-		{#if card_obj["use_img"] == false}
+-->
+
+
+<div class="PlaceholderCard border-0 bg-blue-50" style="width: {card_obj["dimensions"][0]}px; height: {card_obj["dimensions"][1]}px">
+	<div> <!-- left stuff -->
+		<div class="right-side-up">
+			<p class="corner-val">
+				{card_obj["corner-val"]}
+			</p>
+			<img src={card_obj["small_image"]} alt="small">
+		</div>
+	</div>
+	<div class="centerdiv"> <!-- center image-->
+		<img src={card_obj["image"]} alt="center">
+	</div>
+	<div class="right-col"> <!-- right stuff -->
+		<div class="upside-down">
+			<p class="corner-val">
+				{card_obj["corner-val"]}
+			</p>
+			<img src={card_obj["small_image"]} alt="small">
+		</div>
+	</div>
+		<!--{#if card_obj["use_img"] == false}
 			<p>false</p>
 		{:else if card_obj["img_src"] != null}
 			<p> in else if </p>
@@ -39,12 +64,30 @@
 		{:else}
 			<p>in else</p>
 		{/if}
-
+		-->
 </div>
 
-<style>
-	div.test {
-		width:100px;
-		height:100px;
+
+<style lang="css">
+	.PlaceholderCard {
+		display: grid;
+		grid-template-columns: 1fr 3fr 1fr;
+	}
+	.centerdiv {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height:100%;
+	}
+	.corner-val {
+		font-size: 40px;
+	}
+	.right-col {
+		position:relative;
+	}
+	.upside-down {
+		position:absolute;
+		bottom:0;
+		transform: rotate(180deg);
 	}
 </style>
