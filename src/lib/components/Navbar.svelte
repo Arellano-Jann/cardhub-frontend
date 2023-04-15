fde2dc0f9aa5ef8139038efb0f5344fcecbd24fb
slehman133 <43526258+slehman133@users.noreply.github.com>
2023-04-15 16:04:47 -0700

    add lobby route

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,49 +1,69 @@
 <script>
     //import Home from "../../routes/home/+page.svelte";   //Idk if these are right
-    //import HostJoin from "../../routes/hostjoin/+page.svelte"; 
-    //import Search from "../../routes/search/+page.svelte"; 
-    //import About from "../../routes/about/+page.svelte"; 
+    //import HostJoin from "../../routes/hostjoin/+page.svelte";
+    //import Search from "../../routes/search/+page.svelte";
+    //import About from "../../routes/about/+page.svelte";
     let Home = "https://www.google.com/";
     let HostJoin = "https://www.amazon.com/";
     let Search = "https://www.yahoo.com/";
     let About = "https://www.espn.com/";
     let currentPage = Home;
-    function setCurrentPage(page){
+    function setCurrentPage(page) {
         currentPage = page;
     }
 </script>
 
 <nav>
     <ul class="section-links">
-      <li class="section-link" on:click={() => setCurrentPage(Home)} on:keypress={() => setCurrentPage(Home)} class:selected={currentPage == Home}>
-        <a href={Home}>
-          <span class="material-icons">home</span>
-          Home
-        </a>
-      </li>
-      <li class="section-link" on:click={() => setCurrentPage(HostJoin)} on:keypress={() => setCurrentPage(HostJoin)} class:selected={currentPage == HostJoin}>
-        <a href={HostJoin}>
-          <span class="material-icons">person_add</span>
-          Host/Join
-        </a>
-      </li>
-      <li class="section-link" on:click={() => setCurrentPage(Search)} on:keypress={() => setCurrentPage(Search)} class:selected={currentPage == Search}>
-        <a href="{Search}">
-          <span class="material-icons">search</span>
-          Search
-        </a>
-      </li>
-      <li class="section-link" on:click={() => setCurrentPage(About)} on:keypress={() => setCurrentPage(About)} class:selected={currentPage == About}>
-        <a href="{About}">
-          <span class="material-icons">info</span>
-          About
-        </a>
-      </li>
+        <li
+            class="section-link"
+            on:click={() => setCurrentPage(Home)}
+            on:keypress={() => setCurrentPage(Home)}
+            class:selected={currentPage == Home}
+        >
+            <a href={Home}>
+                <span class="material-icons">home</span>
+                Home
+            </a>
+        </li>
+        <li
+            class="section-link"
+            on:click={() => setCurrentPage(HostJoin)}
+            on:keypress={() => setCurrentPage(HostJoin)}
+            class:selected={currentPage == HostJoin}
+        >
+            <a href={HostJoin}>
+                <span class="material-icons">person_add</span>
+                Host/Join
+            </a>
+        </li>
+        <li
+            class="section-link"
+            on:click={() => setCurrentPage(Search)}
+            on:keypress={() => setCurrentPage(Search)}
+            class:selected={currentPage == Search}
+        >
+            <a href={Search}>
+                <span class="material-icons">search</span>
+                Search
+            </a>
+        </li>
+        <li
+            class="section-link"
+            on:click={() => setCurrentPage(About)}
+            on:keypress={() => setCurrentPage(About)}
+            class:selected={currentPage == About}
+        >
+            <a href={About}>
+                <span class="material-icons">info</span>
+                About
+            </a>
+        </li>
     </ul>
-  </nav>  
-  
+</nav>
+
 <style lang="css">
-    nav{
+    nav {
         position: fixed;
         display: flex;
         align-items: center;
@@ -56,14 +76,14 @@
         height: 50px;
     }
 
-    .section-links{
+    .section-links {
         display: flex;
         margin: 0;
         padding: 0;
         list-style: none;
     }
 
-    .section-link{
+    .section-link {
         display: flex;
         flex-grow: 1;
         color: white;
@@ -80,15 +100,15 @@
         transition: background-color 0.1s ease-in-out;
     }
 
-    .section-link:hover{
+    .section-link:hover {
         background-color: #ffffff80;
     }
 
-    .section-link:active{
+    .section-link:active {
         color: red;
     }
 
-    .section-link a .material-icons{
+    .section-link a .material-icons {
         font-size: 16px;
     }
 </style>
