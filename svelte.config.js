ad720bd0b4568c37323c36c7c54b892a935dcb32
dionisio-rubi <rdionisio@nevada.unr.edu>
2023-04-15 10:30:35 -0700

    sample websocks doesnt work fully

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,41 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-node';
+// import { vitePreprocess } from '@sveltejs/kit/vite'; // not sure about this either, check later
+import preprocess from 'svelte-preprocess';
+
+
+// save forpossible fututre use
+// import { Server} from 'socket.io'
+
+// export const webSocketServer = {
+// 	name: 'webSocketServer',
+// 	configerServer(server) {
+// 		const io = new Server(server.httpServer); // might need to change in the future, from vite config
+		
+// 		io.on('connection', (socket) => {
+// 			socket.emit('eventFromServer', 'Hello from the server')})
+// 	}
+// }
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+	kit:{
 		adapter: adapter()
-	},
-	preprocess: vitePreprocess()
+	}
+
+	// preprocess: vitePreprocess(),
+	// kit: {
+	// 	adapter: adapter(),
+	// 	vite: {
+	// 		plugins: [webSocketServer],
+	// 	},
+	// },
+	
 };
 
 export default config;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite'; // not sure about this either, check later
+import { webSocketServer } from './webSocketPluginVite.js';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+
+// export default defineConfig({
+/** @type {import('vite').UserConfig} */
+const config = {
+	server: {
+		port: 3000
+	},
+	preview: {
+		port: 3000
+	},
+	plugins: [sveltekit(), webSocketServer],
+};
+	
+// });
