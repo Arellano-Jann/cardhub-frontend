237e08aade6cc1dba75f1f888691bd48e2b13215
brown-lyssie <abrown@nevada.unr.edu>
2023-04-15 17:23:35 -0700

    deckview changes

--- src/lib/components/DeckView.svelte
+++ src/lib/components/DeckView.svelte
@@ -7,22 +7,26 @@
 	import cards_against_humanity_deck from "$lib/assets/cards_against_humanity/deck.json"
 	let cards_against_humanity_back = cards_against_humanity_deck["back_card"];
 </script>
-
-<div class="deck-grid">
-	<div class="placeholder-wrapper">
-		<PlaceholderCard card_obj={playing_card_back} />
-	</div>
-
-	<div class="placeholder-wrapper">
-		<PlaceholderCard card_obj={cards_against_humanity_back} />
+<div class="deckview" style="width:100p">
+	<div class="deck-grid">
+		<div class="placeholder-wrapper">
+			<PlaceholderCard card_obj={playing_card_back} />
+		</div>	
+		<div class="placeholder-wrapper">
+			<PlaceholderCard card_obj={cards_against_humanity_back} />
+		</div>
 	</div>
 </div>
 
-<style> 
-	.placeholder-wrapper {
-		float:left;
+<style>
+	.deckview {
+		background-color: blue;
 	}
 	.deck-grid {
+		padding: 10px;
 		display:block;
+		display: grid;
+  		grid-gap: 10px;
+  		grid-template-columns: repeat(auto-fill, 140px);
 	}
 </style>
--- src/routes/host/+page.svelte
+++ src/routes/host/+page.svelte
@@ -2,14 +2,13 @@
 	//import cah_deck from "$"
 	import DeckView from "$lib/components/DeckView.svelte"
 	import CardLibrary from "$lib/components/CardLibrary.svelte"
-
+	import Navbar from "../../lib/components/Navbar.svelte";
 	$: selected_cards = 5 + 10;
 
 </script>
-<div id="page">
-	<h1> Host game </h1>
-	<h2> Select Game to Play </h2>
-	<DeckView />
-	<h2> View Cards in Deck </h2>
-	<CardLibrary {selected_cards} />
-</div>
+< Navbar />
+<h1> Host game </h1>
+<h2> Select Game to Play </h2>
+<DeckView />
+<h2> View Cards in Deck </h2>
+<CardLibrary {selected_cards} />
