8939b0371b9492ba6093be35bcd9b9c17b15ee7d
dionisio-rubi <rdionisio@nevada.unr.edu>
2023-04-15 23:10:26 -0700

    buttons work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
 	// import logo from "../../lib/assets/card_hub_logo.svg";
+	function moveJoin(){
+    window.location.href = "/join";
+  }
+
+  function moveHost(){
+    window.location.href = "/host";
+  }
 </script>
 
 <!-- <a href="/home">g</a> -->
@@ -14,11 +21,11 @@
 	<div class="flex flex-col items-center gap-5"> 
 		<h1>Welcome to Card Hub!</h1>
 		<div class="flex flex-col items-center gap-5">
-			<button
+			<button on:click={moveHost}
 			class="w-75 py-2 px-4 border-transparent rounded-md shadown-sm test-sm font-medium text-black bg-red-400 hover:bg-red-500 hover:text-white py-2 px-5 border-2 border-black rounded-lg justify-center"
 			>Host a Game</button>
 
-			<button
+			<button on:click={moveJoin}
 			class="w-75 justify-center py-2 px-4 border-transparent rounded-md shadown-sm test-sm font-medium text-black bg-red-400 hover:bg-red-500 hover:text-white py-2 px-5 border-2 border-black rounded-lg"
 			>Join a Game</button>
 		</div>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,7 +1,14 @@
 <script>
   // import Navbar from "../../lib/components/Navbar.svelte";
-  import logo from "../../lib/assets/card_hub_logo.svg";
+  // import logo from "../../lib/assets/card_hub_logo.svg";
   // import "@fontsource/open-sans";
+  function moveJoin(){
+    window.location.href = "/join";
+  }
+
+  function moveHost(){
+    window.location.href = "/host";
+  }
 </script>
 
 
@@ -11,11 +18,11 @@
   <div class="flex flex-col items-center gap-5"> 
 		<h1>Welcome to Card Hub!</h1>
 		<div class="flex flex-col items-center gap-5">
-			<button
+			<button on:click={moveHost}
 			class="w-75 py-2 px-4 border-transparent rounded-md shadown-sm test-sm font-medium text-black bg-red-400 hover:bg-red-500 hover:text-white py-2 px-5 border-2 border-black rounded-lg justify-center"
 			>Host a Game</button>
 
-			<button
+			<button on:click={moveJoin}
 			class="w-75 justify-center py-2 px-4 border-transparent rounded-md shadown-sm test-sm font-medium text-black bg-red-400 hover:bg-red-500 hover:text-white py-2 px-5 border-2 border-black rounded-lg"
 			>Join a Game</button>
 		</div>
