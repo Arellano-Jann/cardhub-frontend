bb7d92add629117598f4f06ceab4d9b038250fa4
Mamaril-Jeremy <89481128+Mamaril-Jeremy@users.noreply.github.com>
2023-04-16 08:50:44 -0700

    Create +page.svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<template>
+  <div class="container">
+    <h1>About</h1>
+    <p>This is a card simulator app designed for those who want to play with a regular deck of 52 cards, Cards Against Humanity, or even UNO.</p>
+    <p>The app is developed by a team of developers who have a passion for card games. Our goal was to provide a fun and engaging place for players to enjoy their favorite card games and improve their skills at the same time, with no restrictions!</p>
+  </div>
+</template>
+
+<style>
+  h1 {
+    color: white;
+    font-size: 3rem;
+    background-color: #3498db;
+    padding: 20px;
+    margin-bottom: 35px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    background-color: #f1f1f1;
+  }
+
+  p {
+    font-size: 1.2rem;
+    line-height: 1.5;
+    margin-bottom: 20px;
+    padding: 0 20px;
+    font-family: Arial, sans-serif;
+    background-color: #f1f1f1;
+  }
+
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 40px;
+    background-color: white;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+  }
+</style>
